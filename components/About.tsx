0a978529796f467aedc437d983762d5df01cd4d1
SpywarePersues <spywarepersues284@gmail.com>
2022-10-04 08:02:25 +0530

    Changed Color combinations

--- components/About.tsx
+++ components/About.tsx
@@ -8,38 +8,38 @@ import CustomLink from './helpers/Link'
 function About() {
   return (
     <>
-      <div className='flex justify-between p-3 py-10 mx-3 bg-purple-600 md:mx-10 rounded-xl'>
+      <div className='mx-3 flex justify-between rounded-xl bg-blue-600 p-3 py-10 md:mx-10'>
         <div className='w-full'>
           {/* <div className='text-3xl md:text-5xl font-poppins font-semibold text-[#380d79] m-6'>
             About me
           </div> */}
-          <div className='text-[#f8f7fa] mx-2 md:mx-7 md:text-lg'>
+          <div className='mx-2 text-[#f8f7fa] md:mx-7 md:text-lg'>
             I am a 17 year old{' '}
-            <span className='text-purple-900 font-poppins'>
+            <span className='font-poppins text-purple-900'>
               full-stack developer
             </span>{' '}
             with a passion for building things that people{' '}
-            <span className='text-transparent font-poppins bg-gradient-to-r from-red-300 to-red-500 bg-clip-text'>
+            <span className='bg-gradient-to-r from-red-300 to-red-500 bg-clip-text font-poppins text-transparent'>
               love
             </span>
             .
             <br />
             <br />
             I&apos;ve built{' '}
-            <span className='text-blue-200 font-poppins'>
+            <span className='font-poppins text-blue-200'>
               highly scalable
             </span>{' '}
             web applications,{' '}
             <a
               href='https://twitter.com/poet_this'
-              className='text-transparent font-poppins bg-gradient-to-b from-sky-400 to-sky-200 bg-clip-text'
+              className='bg-gradient-to-b from-sky-400 to-sky-200 bg-clip-text font-poppins text-transparent'
             >
               a Twitter bot
             </a>{' '}
             that serves more than 20 thousand people and a{' '}
             <a
               href='https://github.com/dhravya/spacebot6'
-              className='text-transparent bg-gradient-to-b from-gray-900 to-gray-600 bg-clip-text font-poppins'
+              className='bg-gradient-to-b from-gray-900 to-gray-600 bg-clip-text font-poppins text-transparent'
             >
               discord bot
             </a>{' '}
@@ -51,13 +51,13 @@ function About() {
             In my free time, I love to Cycle, listen to music,{' '}
             <a
               href='https://instagram.com/dhravyaplaysmusic'
-              className='font-semibold text-transparent font-poppins bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100 bg-clip-text'
+              className='bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100 bg-clip-text font-poppins font-semibold text-transparent'
             >
               play the Guitar
             </a>{' '}
             and Table tennis.
             <br />
-            <div className='font-semibold text-transparent font-poppins bg-gradient-to-r from-sky-400 to-blue-500 bg-clip-text'>
+            <div className='bg-gradient-to-r from-sky-400 to-blue-500 bg-clip-text font-poppins font-semibold text-transparent'>
               <CustomLink href='/about'>Read more about me</CustomLink>
             </div>
           </div>
